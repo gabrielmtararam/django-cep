c0d24080aa1241ebbe614d3d253666d43cac26fd
willtecti <wilton.tecti@gmail.com>
2019-03-30 14:37:59 -0300

    adicionando caminho relativo ao importe da view

--- cep/urls.py
+++ cep/urls.py
@@ -4,7 +4,7 @@ try:
 except ImportError:  # deprecated since Django 1.4
     from django.conf.urls.defaults import patterns, url
 
-from views import addressGet
+from .views import addressGet
 
 urlpatterns = patterns('',
                        url(r'^(?P<zipcode>[\w-]+)/$', addressGet, name='address'),
